83f51f0f44756ec1075a5c04668a386d0ad70013
RouxRhett <nsch.4871@gmail.com>
2021-09-22 02:29:53 +0900

    p100

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,19 +1,19 @@
 class HelloController < ApplicationController
-  protect_from_forgery
+
 
   def index
     if request.post? then
       @title = 'Result'
-      @msg = 'you Typed: ' + params['input1'] + '.'
-      @value = params['input1']
+      if params['check1'] then
+        @msg = 'you Checked!!'
+      else
+        @msg = 'not checked...'
+      end
     else
-      @title = 'index'
-      @msg = 'type text...'
-      @value = ''
+      @title = 'Index'
+      @msg = 'check it...'
     end
   end
 
-  def other
-    redirect_to action: :index, params:{'msg': 'from other page'}
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :tweets
   get 'hello/index'
   get 'hello', to: 'hello#index'
   get 'hello/other'
-  post 'hello', to:'hello#index'
+  post 'hello', to: 'hello#index'
   post 'hello/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
