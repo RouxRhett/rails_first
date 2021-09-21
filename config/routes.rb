78078a8fcbedc77948ad9c461f1ada6c00ca27a5
RouxRhett <nsch.4871@gmail.com>
2021-09-21 23:46:23 +0900

    p91

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'hello/index'
   get 'hello', to: 'hello#index'
   get 'hello/other'
+  post 'hello', to:'hello#index'
+  post 'hello/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
