c4452957d27007ebfe2a5963982d7d7438a3b038
RouxRhett <nsch.4871@gmail.com>
2021-09-23 22:48:31 +0900

    p133,msgboard-add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'msgboard', to: 'msgboard#index'
+  post 'msgboard', to: 'msgboard#index'
+  get 'msgboard/index'
+  post 'msgboard/index'
+
   get 'hello/index'
   get 'hello', to: 'hello#index'
   get 'hello/other'
