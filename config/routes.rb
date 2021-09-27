56141f649ee9b63c2234a52fef7f7f103666ccdc
RouxRhett <nsch.4871@gmail.com>
2021-09-27 16:39:13 +0900

    p187,editpage

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get 'people/add'
   post 'people/add', to: 'people#create'
 
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
+
   get 'people', to: 'people#index'
   get 'people/:id', to: 'people#show'
 end
